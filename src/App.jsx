f58a131313328989ffc5839b91d82fc25f8cc768
ILIAink~ <ysk.iliakhalighi@gmail.com>
2024-03-08 13:09:52 -0500

    More CSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,7 +15,7 @@ function App() {
       </div>
 
       <div
-        className="bg-custom-purple-100 h-screen flex flex-1 justify-center items-center"
+        className="bg-custom-purple-100 h-screen flex flex-1 flex-col justify-center items-center"
         style={{ height: "calc(100vh - navbarHeight)" }}
       >
         <h1>Welcome to LeetStats</h1>
