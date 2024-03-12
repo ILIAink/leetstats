bfc75510550dfe8a6f95d9caf3ee1a87ab0f19d2
rdwrds <r.edwards1492@gmail.com>
2024-03-12 15:55:40 -0500

    init

---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Navbar()
+{
+    return(
+        <div className="fixed shadow-md top-0 w-full z-50 bg-custom-purple-700 text-white py-4">
+        <nav className="container mx-auto flex justify-between px-4 md:px-0">
+          <div>LeetStats</div>
+          <div>{/* Navigation Links or Icons */}</div>
+        </nav>
+      </div>
+    );
+}
+
+export default Navbar;
