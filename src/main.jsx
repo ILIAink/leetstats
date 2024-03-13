2bb3ca476f868b679241a9a0df4cdc4fd812e904
rdwrds <r.edwards1492@gmail.com>
2024-03-13 18:56:24 -0500

    init apollo client

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Navbar from "./Navbar.jsx";
 import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+// Define the URL for the GraphQL API
+const endpoint = 'https://countries.trevorblades.com/'; // Replace this with the actual GraphQL endpoint
+//https://github.com/trevorblades/countries - docs for countries test API
+
+const client = new ApolloClient({
+    uri: endpoint,
+    cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
+  <ApolloProvider client={client}>
     <Navbar/>
     <App />
+  </ApolloProvider>
   </>
 );
