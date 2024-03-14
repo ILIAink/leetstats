46dc1f40586a6cd0b60a0a0dab75672ed9186562
rdwrds <r.edwards1492@gmail.com>
2024-03-13 19:37:21 -0500

    commiting cuz he told me to

--- src/DataDisplay.jsx
+++ src/DataDisplay.jsx
@@ -1,31 +1,52 @@
 import React from "react";
 import { ReactDOM } from "react";
 import { useState } from "react";
+import { useQuery, gql } from '@apollo/client';
 
+const GET_COUNTRIES = gql`
+    query GetCountries
+    {
+        name
+        states
+        {
+            name
+        }
+    }
+`;
 
+function ActualDataDisplay()
+{
+    const {loading, error, data} = useQuery(GET_COUNTRIES);
 
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return(
+        <p>{data}</p>
+    );
+};
 
 function DataDisplay()
 {
-    //const [country, setCountry] = useState('');
 
     return(
         <div className="max-w-1/2 shadow-lg h-fit pb-5 border-1 rounded-md text-center bg-custom-purple-300 ">
             <h1 className="pt-3 px-3 font-semibold">Welcome to LeetStats</h1>
             <div className="mb-3">
-            <p1 className="">
+            <p1 className="">   
                 <span className="italic font-semibold">All </span> 
                 your stats in one place.</p1>
             </div>
 
             <div className="pt-1 shadow-md bg-custom-purple-200 border-1 rounded-md w-3/4 content-center m-auto">
                     <div className="pb-2 pt-1">
+                        <ActualDataDisplay/>
                     <p1 className="font-medium">Country</p1>
                         <form className="pt-2" onSubmit={
                             (e) => {
                                 //forms get submitted, not text input :)
                                 e.preventDefault();
-                                console.log(e)
+                                console.log("cheese")
                             }
                         }>
                         <input type="text" name="Country" className=" border-1 rounded-md w-3/4 max-w-40" />
@@ -36,4 +57,4 @@ function DataDisplay()
     );
 }
 
-export default DataDisplay
+export default DataDisplay;
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Navbar from "./Navbar.jsx";
 import "./index.css";
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client';
 
 // Define the URL for the GraphQL API
 const endpoint = 'https://countries.trevorblades.com/'; // Replace this with the actual GraphQL endpoint
 //https://github.com/trevorblades/countries - docs for countries test API
 
 const client = new ApolloClient({
-    uri: endpoint,
+    uri: "https://countries.trevorblades.com/",
     cache: new InMemoryCache(),
-})
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
