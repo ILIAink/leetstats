3b5edb1926386652b21a3b31b7e91617d12a6893
ilia <ysk.iliakhalighi@gmail.com>
2024-03-14 16:19:42 -0400

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import "./Login.jsx"
-import Login from "./Login";
+import { useState, useEffect } from "react";
+import Login from "./components/Login";
+import axios from "axios";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
+  // Additional state to store the user's profile data
+  const [userProfile, setUserProfile] = useState(null);
 
   return (
     <>
+      <Navbar />
       <div
         className="bg-custom-purple-100 h-screen flex flex-1 justify-center items-center"
         style={{ height: "calc(100vh - navbarHeight)" }}
       >
-
-      <Login />
+        <Login />
       </div>
-
     </>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,16 +11,22 @@ function Login() {
           your stats in one place.
         </p1>
       </div>
-
-      <div className="pt-1 shadow-md bg-custom-purple-200 border-1 rounded-md w-3/4 content-center m-auto">
-        <div className="pb-2 pt-1">
-          <p1 className="font-medium">Username</p1>
-          <form className="pt-2">
+      <div className="pt-1 h-40 flex justify-center items-center shadow-md bg-custom-purple-200 border-1 rounded-md w-3/4 content-center m-auto">
+        <div className="flex flex-col items-start pb-2 pt-1">
+          <p1 className="font-medium">Username: </p1>
+          <form className="pt-2 flex gap-2">
             <input
               type="text"
               name="Username"
               className="p-2 border-1 rounded-md"
             />
+            <button
+              onClick={(e) => e.preventDefault()}
+              className="p-2 rounded-md bg-slate-500"
+              type="submit"
+            >
+              Fetch
+            </button>
           </form>
         </div>
       </div>
