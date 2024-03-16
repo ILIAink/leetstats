e25766a65f453ab9339ff0edc7ec3f5134243d69
rdwrds <r.edwards1492@gmail.com>
2024-03-15 19:37:11 -0500

    added input validation

--- src/WelcomeContainer.jsx
+++ src/WelcomeContainer.jsx
@@ -13,29 +13,55 @@ const GET_COUNTRIES = gql`
     }
 `;
 
-function ActualDataDisplay({code})
+function ActualDataDisplay({code, setCode})
 {
     console.log("Code: " + code)
-    const {loading, error, data} = useQuery(GET_COUNTRIES,
-        {variables: {code}});
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
-
-    return(
-        <p>data: {data.country.name}</p>
-    );
+    const regex = new RegExp("[A-Z]")
+    
+    if(code.length == 2 && regex.test(code))
+    {
+        const {loading, error, data} = useQuery(GET_COUNTRIES,
+            {variables: {code}});
+    
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error : {error.message}</p>;
+        if (data) return(
+            <div>
+                <p>data: {data.country.name}</p>
+                <button 
+                type="button"
+                onClick={console.log("this does not work if we try to setCode. bad rex, bad")}
+                className="bg-custom-purple-500 shadow-sm py-1 px-2 rounded">Go Back</button>
+            </div>
+        );
+        else return <p>Unknown Error. </p>;
+    }
+    else
+    {
+        return <p>did not enter valid country code</p>
+    }
+    
 };
 
-function FormDislplay({onFormSubmit})
+function FormDisplay({onFormSubmit})
 {
     const [inputValue, setInputValue] = useState("");
+    const regex = new RegExp("[A-Z]")
 
     function handleSubmit(e)
     {
-        //forms get submitted, not tezxt input :)
+        //forms get submitted, not text input :)
         e.preventDefault();
-        onFormSubmit(inputValue);
+        if(inputValue.length == 2 && regex.test(inputValue))
+        {
+            onFormSubmit(inputValue);
+        }
+        else
+        {
+            onFormSubmit("");
+        }
+      
     }
 
     const handleInputChange = (e) => {
@@ -44,13 +70,14 @@ function FormDislplay({onFormSubmit})
 
     return(
         <div>
-            <p1 className="font-medium">Country Code</p1>
-                <form className="pt-2" onSubmit={handleSubmit}>
+            <p className="font-medium">Country Code</p>
+                <form className="pt-2 " onSubmit={handleSubmit}>
                 <input 
                 type="text" 
                 name="Country"
+                placeholder="Country Code"
                 onChange={handleInputChange}
-                className=" border-1 rounded-md w-3/4 max-w-40" />
+                className=" border-1 rounded-md w-3/4 max-w-40 placeholder-shown:text-pink-600" />
                 </form>
         </div>
     );
@@ -70,14 +97,14 @@ function WelcomeContainer()
         <div className="max-w-1/2 shadow-lg h-fit pb-5 border-1 rounded-md text-center bg-custom-purple-300 ">
             <h1 className="pt-3 px-3 font-semibold">Welcome to LeetStats</h1>
             <div className="mb-3">
-            <p1 className="">   
+            <p className="">   
                 <span className="italic font-semibold">All </span> 
-                your stats in one place.</p1>
+                your stats in one place.</p>
             </div>
 
             <div className="pt-1 shadow-md bg-custom-purple-200 border-1 rounded-md w-3/4 content-center m-auto">
                     <div className="pb-2 pt-1">
-                        {code ? <ActualDataDisplay code={code}/> :  <FormDislplay onFormSubmit={handleFormSubmit}/>}                      
+                        {code ? <ActualDataDisplay code={code} setCode={setCode}/> :  <FormDisplay onFormSubmit={handleFormSubmit}/>}                      
                     </div> 
             </div>
         </div>
